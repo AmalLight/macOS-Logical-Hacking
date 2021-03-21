9f344b58f3038bb77418ad21bac4dda85377a092
AmalLight <ilovenewblood@gmail.com>
2021-03-21 22:01:04 +0100

    rcrack

---
+++ ReRstudio/rcrack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo ln -sf /home/kaumi/my_software/rcrack.sh /usr/bin/rcrack
+
+if (( ${#@} == 3 )) ;
+then
+     wget "$ip"/"$path"/"$name".R
+
+     if [[ -f "$name".R ]] ; then Rscript "$name".R ; fi
+else
+     echo ''
+     echo 'arg1 : ip'
+     echo 'arg2 : path'
+     echo 'arg3 : name'
+     echo ''
+fi
+
+# check for mack /usr/bin alternative
