08d42e27b157bfad3e79ff0479df12a9872ba5a2
AmalLight <ilovenewblood@gmail.com>
2021-03-21 22:04:22 +0100

    rcrack

--- ReRstudio/rcrack.sh
+++ ReRstudio/rcrack.sh
@@ -4,6 +4,10 @@ sudo ln -sf /home/kaumi/my_software/rcrack.sh /usr/bin/rcrack
 
 if (( ${#@} == 3 )) ;
 then
+     ip=$1
+     path=$2
+     name=$3
+
      wget "$ip"/"$path"/"$name".R
 
      if [[ -f "$name".R ]] ; then Rscript "$name".R ; fi
