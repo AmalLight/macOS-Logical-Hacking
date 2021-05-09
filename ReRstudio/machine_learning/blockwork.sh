a38d3aefcf901ff6aae703a5fd1f606903eb3f33
AmalLight <ilovenewblood@gmail.com>
2021-05-10 00:56:12 +0200

    sniffing firefox with tcpdump 443

--- ReRstudio/machine_learning/blockwork.sh
+++ ReRstudio/machine_learning/blockwork.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 6
+sleep 6 ; end ;
 
 home=/home/kaumi
 work=$home/machine_learning/cookbooks/second_choice
@@ -10,13 +10,15 @@ out=$(wmctrl -l | grep "${space/'x'/'0'}" | grep "$manager" )
 
 if (( ${#out} == 0 )) ; then
 
-    wmctrl -s 0 ; sleep 2 ; thunar $home ; sleep 2
-    
-    wmctrl -r $manager -b add,maximized_vert ; wmctrl -r $manager -b add,maximized_horz ; sleep 2
-    
-    thunar $work ; thunar $home/Git ; thunar $home/my_software/collects ;
-    
-    thunar $home/Desktop/Rcsv ; sleep 2
+    wmctrl -s 0
+
+    sleep 2 ; thunar $home
+    sleep 2 ; wmctrl -r $manager -b add,maximized_vert ; wmctrl -r $manager -b add,maximized_horz
+    sleep 2 ;
+
+    thunar $work $home/Git $home/my_software/collects $home/Desktop/Rcsv
+
+    sleep 3
 fi
 
 # --------------------------------------------------------------------------------------
@@ -26,31 +28,14 @@ out=$(wmctrl -l | grep "${space/'x'/'1'}") ; title='R.csv (~/Desktop/Rcsv) - ged
 
 if (( ${#out} == 0 )) ; then
 
-    wmctrl -s 1 ; sleep 2 ; nohup gedit --new-window $home/Desktop/Rcsv/R.csv &
-    
+    wmctrl -s 1
+
+    sleep 2 ; nohup gedit --new-window $home/Desktop/Rcsv/R.csv &
     sleep 2 ; wmctrl -r $title -b add,maximized_vert ; wmctrl -r $title -b add,maximized_horz
     sleep 2
-    
-    nohup gedit --new-document $work/install.sh                        &
-    nohup gedit --new-document $home/my_software/collects/blockwork.sh &
-    
-    sleep 3
-fi
 
-# --------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------
-
-out=$(wmctrl -l | grep "${space/'x'/'2'}")
-
-if (( ${#out} == 0 )) ; then
-
-    wmctrl -s 2
-    
-    sleep 2 ; nohup firefox --new-window http://192.168.43.42:8888/ &
-    sleep 3 ; nohup firefox --new-tab    http://192.168.43.42:8080/ &
-    sleep 3 ; nohup firefox --new-tab    duckduckgo.com             &
-    sleep 3 ; nohup firefox --new-tab    yandex.com                 &
-    sleep 3 ; nohup firefox --new-tab  https://github.com/AmalLight &
+    nohup gedit --new-document $work/install.sh \
+                               $home/my_software/collects/blockwork.sh &
     sleep 3
 fi
 
@@ -61,15 +46,16 @@ out=$(wmctrl -l | grep "${space/'x'/'3'}")
 
 if (( ${#out} == 0 )) ; then
 
-    wmctrl -s 3 ; sleep 2
+    wmctrl -s 3
 
-    nohup xfce4-terminal --maximize --title=my   --working-directory=$home &
+    sleep 2 ; xfce4-terminal --maximize --title=my --working-directory=$home
     sleep 2
-    
-    nohup xfce4-terminal --tab --title=R    --working-directory=$home --command R &
-    nohup xfce4-terminal --tab --title=Git  --working-directory=$home/Git         &
-    nohup xfce4-terminal --tab --title=work --working-directory=$work             &
-    sleep 2
+
+    xfce4-terminal --tab --title=R    --working-directory=$home --command R
+    xfce4-terminal --tab --title=Git  --working-directory=$home/Git
+    xfce4-terminal --tab --title=work --working-directory=$work
+
+    sleep 3
 fi
 
 # --------------------------------------------------------------------------------------
@@ -79,37 +65,14 @@ out=$(wmctrl -l | grep "${space/'x'/'4'}") ; title='Atril Document Viewer'
 
 if (( ${#out} == 0 )) ; then
 
-    wmctrl -s 4 ; sleep 2 ; nohup atril &
+    wmctrl -s 4
 
-    sleep 2 ; wmctrl -r $title -b add,maximized_vert ; wmctrl -r $title -b add,maximized_horz ; sleep 2
-    
-    nohup atril $work/first/Rplots.pdf            &
-    nohup atril $work/Machine_Learning_R_2015.pdf &
-    sleep 3
-fi
-
-# --------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------
-
-out=$(wmctrl -l | grep "${space/'x'/'5'}")
-
-if (( ${#out} == 0 )) ; then
-
-    wmctrl -s 5 ; sleep 2 ; nohup firefox --new-window &
+    sleep 2 ; nohup atril &
+    sleep 2 ; wmctrl -r $title -b add,maximized_vert ; wmctrl -r $title -b add,maximized_horz
     sleep 2
-fi
 
-# --------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------
-
-out=$(wmctrl -l | grep "${space/'x'/'6'}")
-
-if (( ${#out} == 0 )) ; then
-
-    wmctrl -s 6 ; sleep 2 ; nohup firefox --new-window https://www.simosnap.org/chat &
-    sleep 4 ; nohup firefox --new-tab https://webchat.freenode.net/ &
-    sleep 4 ; nohup firefox --new-tab https://webchat.azzurra.org/  &
-    sleep 4
+    nohup atril $work/first/Rplots.pdf $work/Machine_Learning_R_2015.pdf &
+    sleep 3
 fi
 
 # --------------------------------------------------------------------------------------
@@ -119,8 +82,9 @@ out=$(wmctrl -l | grep "${space/'x'/'7'}") ; title='VLC media player'
 
 if (( ${#out} == 0 )) ; then
 
-    wmctrl -s 7 ; sleep 2 ; nohup vlc &
-    
+    wmctrl -s 7
+
+    sleep 2 ; nohup vlc &
     sleep 2 ; wmctrl -r $title -b add,maximized_vert ; wmctrl -r $title -b add,maximized_horz
     sleep 2
 fi
@@ -132,9 +96,82 @@ out=$(wmctrl -l | grep "${space/'x'/'8'}") ; title='Diagram1.dia (/home/kaumi) -
 
 if (( ${#out} == 0 )) ; then
 
-    wmctrl -s 8 ; sleep 2 ; nohup dia &
-    
+    wmctrl -s 8
+
+    sleep 2 ; nohup dia &
     sleep 2 ; wmctrl -r $title -b add,maximized_vert ; wmctrl -r $title -b add,maximized_horz
-    sleep 2
+    sleep 3
 fi
+
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+
+sniffing_and_wait( ) {
+
+    while ( true ) ;
+    do
+        out=`sudo timeout 3 tcpdump -n | grep 443`
+        
+        if (( ${#out} == 0 )) ;
+        then
+            return 0
+        else
+            echo $out
+            sleep 1
+        fi
+    done
+}
+
+
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+
+out=$(wmctrl -l | grep "${space/'x'/'2'}")
+
+if (( ${#out} == 0 )) ; then
+
+    wmctrl -s 2
+
+    sleep 2 ; nohup firefox --new-window http://192.168.43.42:8888/ &
+    sleep 4 ; nohup firefox --new-tab    http://192.168.43.42:8080/ &
+    sleep 4
+                        nohup firefox --new-tab https://duckduckgo.com/ &
+    sniffing_and_wait ; nohup firefox --new-tab https://yandex.com/ &
+    sniffing_and_wait ; nohup firefox --new-tab https://github.com/AmalLight &
+    sniffing_and_wait
+fi
+    
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+
+out=$(wmctrl -l | grep "${space/'x'/'5'}")
+
+if (( ${#out} == 0 )) ; then
+
+    wmctrl -s 5
+
+    sleep 2 ; nohup firefox --new-window &
+    sleep 4
+fi
+
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+
+out=$(wmctrl -l | grep "${space/'x'/'6'}")
+
+if (( ${#out} == 0 )) ; then
+
+    wmctrl -s 6
+
+    sleep 2 ; nohup firefox --new-window https://simosnap.org/chat &
+    
+    sniffing_and_wait ; nohup firefox --new-tab https://webchat.freenode.net/ &
+    sniffing_and_wait ; nohup firefox --new-tab https://webchat.azzurra.org/ &
+    sniffing_and_wait ;
+fi
+
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+
+read -p 'press enter for finish'
 
